12062d64f073eaa099e3260bd00275db978e21fb
DLitvinenko <dimaboblitvinenko@gmail.com>
2023-06-16 10:37:55 +0300

    dz

---
+++ script.js
@@ -0,0 +1,10 @@
+
+let tempC = 15;
+let tempF = tempC + 32 * (9 / 5);
+console.log('температура в цельсиях = ' + tempC);
+console.log('температура в Фаренгейтах = ' + tempF);
+
+let nameUser = 'Dmitry';
+let admin = '' + nameUser;
+admin = nameUser;
+console.log(admin);
